b752f2183a6f5cea893d6760b1e7b40b153df193
Jonatan Frederico <jonatanfrederico@gmail.com>
2020-10-11 13:50:44 -0300

    Remoção de um componente de estilização

--- website/src/components/App/index.tsx
+++ website/src/components/App/index.tsx
@@ -4,17 +4,13 @@ import Home from '../../pages/Home';
 import Login from '../../pages/Login';
 import Register from '../../pages/Register';
 
-import { Container } from './styles';
-
 const App: React.FC = () => {
   return (
-    <Container>
-      <BrowserRouter>
-        <Route exact path="/" component={Home}/>
-        <Route path="/register" component={Register}/>
-        <Route path="/login" component={Login}/>
-      </BrowserRouter>
-    </Container>
+    <BrowserRouter>
+      <Route exact path="/" component={Home}/>
+      <Route path="/register" component={Register}/>
+      <Route path="/login" component={Login}/>
+    </BrowserRouter>
   );
 };
 
